d434cc17de16d21c418f22b0b8530c0f80adb7e8
Valène Renaud <valene.renaud@gmail.com>
2024-12-26 21:26:36 +0100

    feat(Frontend): setup React with Vite and Tailwind CSS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="mx-auto my-4 max-w-sm rounded-2xl border bg-zinc-100 p-4 shadow-xl">
+      <p>Projet</p>
+    </div>
+  );
+}
+
+export default App;
