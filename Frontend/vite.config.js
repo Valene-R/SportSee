2107811015b5833101e36e11131a53fc1fe67490
Valène Renaud <valene.renaud@gmail.com>
2025-01-02 16:23:19 +0100

    chore(config): improve project setup for page rendering

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
+    postcss: "./postcss.config.js",
   },
-})
+  server: {
+    host: "0.0.0.0", // Allow external access
+    port: 5173, // Force port 5173
+    strictPort: true, // Prevent Vite from changing ports automatically
+  },
+});
