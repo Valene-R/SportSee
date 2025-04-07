ac95e3d6eafff1f59858a4714eb425f5267244d6
Valène Renaud <valene.renaud@gmail.com>
2025-04-07 21:15:24 +0200

    chore(NotFound): improve navigation using Link instead of anchor

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /**
  * Display a 404 error page for unmatched routes
  * @returns {JSX.Element} The NotFound component with an error message and a link to return to the home page
@@ -7,9 +9,9 @@ const NotFound = () => {
     <div className="flex h-3/4 flex-col items-center justify-center text-center">
       <h1 className="text-4xl font-bold text-red-500">404 - Page non trouvée</h1>
       <p>La page que vous recherchez n&apos;existe pas.</p>
-      <a href="/" className="mt-6 font-bold text-black hover:underline">
+      <Link to="/" className="mt-6 font-bold text-black hover:underline">
         Retour à l'accueil
-      </a>
+      </Link>
     </div>
   );
 };
